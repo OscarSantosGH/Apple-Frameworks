e2871c0a5c052d906cc15f1b2e08a7de205ec6e3
Oscar Santos <oscarsantos.go@gmail.com>
2020-11-04 06:15:16 -0500

    rename: ContentView -> FrameworksGridView

---
+++ Apple Frameworks/FrameworksGridView.swift
@@ -0,0 +1,20 @@
+//
+//  FrameworksGridView.swift
+//  Apple Frameworks
+//
+//  Created by Oscar Santos on 11/4/20.
+//
+
+import SwiftUI
+
+struct FrameworksGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FrameworksGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        FrameworksGridView()
+    }
+}
