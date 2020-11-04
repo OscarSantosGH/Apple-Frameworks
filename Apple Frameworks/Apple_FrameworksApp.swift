0088fbc694c9eba328a44f7f44fe2b8688ed14f6
Oscar Santos <oscarsantos.go@gmail.com>
2020-11-04 05:58:14 -0500

    Initial Commit

---
+++ Apple Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple Frameworks
+//
+//  Created by Oscar Santos on 11/4/20.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
